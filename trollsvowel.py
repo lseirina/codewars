983ae3268752ae5ebaf060dc23ba7d62df8e25d8
root <lseirina87@gmail.com>
2023-02-16 22:03:07 +0200

    add codewars trollsvowels

---
+++ trollsvowel.py
@@ -0,0 +1,11 @@
+txt =  "This website is for losers LOL!"
+def remove_vowels(a):
+   
+    for i in a.lower():
+        if i in ("e","u", "i", "o", "a"): 
+            a = a.replace(i, "")
+    return a
+
+
+print(remove_vowels(txt))
+            
