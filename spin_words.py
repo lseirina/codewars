2c10db0abcfb816b0845a119ee6c01fbe81492b3
root <lseirina87@gmail.com>
2023-02-22 22:05:34 +0200

    add codewars spin words

---
+++ spin_words.py
@@ -0,0 +1,15 @@
+#spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw" 
+def spin_words(words):
+    split_words = words.split(" ")
+    result = []
+    for word in split_words:
+        if len(word) >= 5:
+            result.append(word[::-1])
+        else:
+            result.append(word)
+            
+    return " ".join(result)
+
+print(spin_words("this is another test"))  
+    
+    
